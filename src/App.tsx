ae922dfa5e0be6415a08adfe4f13db6b04486c29
PhelpsCole <adrmsk@mail.ru>
2022-09-19 14:14:54 +0300

    React project with webpack using code splitting

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./styles/index.scss";
+import {Suspense, useState, useContext} from 'react'
+import {Routes, Route, Link} from 'react-router-dom'
+import {AboutPageAsync} from './pages/AboutPage/AboutPage.async'
+import {MainPageAsync} from './pages/MainPage/MainPage.async'
+import {ThemeContext, Theme} from './theme/ThemeContext'
+import {useTheme} from './theme/useTheme'
+import {classNames} from './helpers/classNames/classNames'
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme()
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О нас</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
