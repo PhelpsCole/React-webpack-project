1fd899aa07c7b334bbaaf09cb440ce96e34d1cd9
PhelpsCole <adrmsk@mail.ru>
2022-09-29 13:12:55 +0300

    ErrorBoundary, ErrorPage, BundleAnalyzer

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,26 @@
+import { classNames } from 'shared/lib/helpers/classNames/classNames';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+    className?: string;
+}
+
+export const PageError = ({ className }: PageErrorProps) => {
+    const { t } = useTranslation();
+
+    const reloadPage = () => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+    };
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            <h1>{t('Произошла непредвиденная ошибка')}</h1>
+            <Button onClick={reloadPage}>
+                {t('Обновить страницу')}
+            </Button>
+        </div>
+    );
+};
