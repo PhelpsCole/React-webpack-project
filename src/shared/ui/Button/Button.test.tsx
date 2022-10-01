44f884476b8d577d5d3dbaccdf96282319777498
PhelpsCole <adrmsk@mail.ru>
2022-10-01 13:36:49 +0300

    Fix lint:css tests and storybook

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -3,10 +3,12 @@ import { Button, ThemeButton } from './Button';
 
 describe('Button', () => {
     test('Simple test', () => {
+        // eslint-disable-next-line i18next/no-literal-string
         render(<Button>TEST</Button>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
     test('Clear theme', () => {
+        // eslint-disable-next-line i18next/no-literal-string
         render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
         screen.debug();
