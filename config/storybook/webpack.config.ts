4ef7aa3f502ee814d0907d8389f35a1b94745ead
PhelpsCole <adrmsk@mail.ru>
2022-10-11 08:30:58 +0300

    Fixed absolute import paths

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -28,5 +28,9 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config.plugins.push(new DefinePlugin({
         __IS_DEV__: true,
     }));
+    config.resolve.modules = [
+        path.resolve(__dirname, '../../src'),
+        'node_modules',
+    ];
     return config;
 };
